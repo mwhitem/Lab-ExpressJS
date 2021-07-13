97fb58399a5ecbb59ecf63a0d634709993ea1050
Madison White <mvwhite32@gmail.com>
2021-07-13 15:07:11 -0500

    append instead of write

--- server/server.js
+++ server/server.js
@@ -33,7 +33,7 @@ app.post("/formsubmissions", (req, res) => {
   } else {
     nameArr.push({ name: req.body.name, email: req.body.email });
 
-    fs.writeFile("form.json", JSON.stringify(nameArr), function (err) {
+    fs.appendFile("form.json", JSON.stringify(nameArr), function (err) {
       if (err) throw err;
     });
   }
